40dcdf73b1fb303753afd8a1b7cf88347535ecbb
Ian Cornett <v-iacorn@microsoft.com>
2017-07-30 21:06:10 -0700

    Add missing Interfaces and remove ignored files.

---
+++ src/main/java/edu/wgu/scheduler/models/ICustomer.java
@@ -0,0 +1,10 @@
+package edu.wgu.scheduler.models;
+
+/**
+ * Created by Ian Cornett - icornet@wgu.edu on 7/30/2017 at 20:47.
+ * Part of Project: scheduler
+ * <p>
+ * Student ID: 000292065
+ */
+public interface ICustomer {
+}
