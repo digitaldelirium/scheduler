2178ed7f0f89431c666b1cb70751f530a0b3a465
Ian Cornett <icornet@wgu.edu>
2017-09-02 21:26:57 -0700

    extract View objects from encapsulating Property classes

--- src/edu/wgu/scheduler/models/AppointmentView.java
+++ src/edu/wgu/scheduler/models/AppointmentView.java
@@ -1,10 +1,156 @@
 package edu.wgu.scheduler.models;
 
+import javafx.beans.property.*;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 /**
- * Created by Ian Cornett - icornet@wgu.edu on 9/2/17.
+ * Created by Ian Cornett - icornet@wgu.edu on 7/30/2017 at 00:43.
  * Part of Project: scheduler
  * <p>
  * Student ID: 000292065
  */
-public class AppointmentView {
+public class AppointmentView implements IAppointmentView {
+    private final ZonedDateTime createdDate;
+    private final String createdBy;
+    // Interface needs these components
+    private ReadOnlyStringProperty title;
+    private ReadOnlyStringProperty description;
+    private ReadOnlyStringProperty location;
+    private ReadOnlyStringProperty contact;
+    private ReadOnlyStringProperty url;
+    private ReadOnlyStringProperty customerName;
+    private ReadOnlyObjectProperty<ZonedDateTime> start;
+    private ReadOnlyObjectProperty<ZonedDateTime> end;
+    private ReadOnlyProperty<ZonedDateTime> lastUpdated;
+
+    /***
+     *
+     * @param title
+     * @param description
+     * @param location
+     * @param contact
+     * @param url
+     * @param customerName
+     * @param start
+     * @param end
+     * @param createDate
+     * @param createdBy
+     * @param lastUpdate
+     */
+    public AppointmentView(String title, String description, String location, String contact, String url, String customerName, Timestamp start, Timestamp end, Timestamp createDate, String createdBy, Timestamp lastUpdate) {
+        this.title = new SimpleStringProperty(title);
+        this.description = new SimpleStringProperty(description);
+        this.location = new SimpleStringProperty(location);
+        this.contact = new SimpleStringProperty(contact);
+        this.customerName = new SimpleStringProperty(customerName);
+        this.start = new SimpleObjectProperty<>(ZonedDateTime.ofInstant(start.toInstant(), ZoneId.systemDefault()));
+        this.end = new SimpleObjectProperty<>(ZonedDateTime.ofInstant(end.toInstant(), ZoneId.systemDefault()));
+        this.lastUpdated = new SimpleObjectProperty<>(ZonedDateTime.ofInstant(lastUpdate.toInstant(), ZoneId.systemDefault()));
+        this.url = new SimpleStringProperty(url);
+        this.createdDate = ZonedDateTime.ofInstant(createDate.toInstant(), ZoneId.systemDefault());
+        this.createdBy = createdBy;
+
+    }
+
+    public String getTitle() {
+        return title.getValue();
+    }
+
+    ReadOnlyStringProperty titleProperty() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description.getValue();
+    }
+
+    ReadOnlyStringProperty descriptionProperty() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location.getValue();
+    }
+
+    ReadOnlyStringProperty locationProperty() {
+        return location;
+    }
+
+    public String getContact() {
+        return contact.getValue();
+    }
+
+    ReadOnlyStringProperty contactProperty() {
+        return contact;
+    }
+
+    public String getUrl() {
+        return url.getValueSafe();
+    }
+
+    ReadOnlyStringProperty urlProperty() {
+        return url;
+    }
+
+    public String getCustomerName() {
+        return customerName.getValue();
+    }
+
+    ReadOnlyStringProperty customerNameProperty() {
+        return customerName;
+    }
+
+    public ZonedDateTime getStart() {
+        return ZonedDateTime.ofInstant(start.getValue().toInstant(), ZoneId.systemDefault());
+    }
+
+    ReadOnlyProperty<ZonedDateTime> startProperty() {
+        return start;
+    }
+
+    public ZonedDateTime getEnd() {
+        return ZonedDateTime.ofInstant(end.getValue().toInstant(), ZoneId.systemDefault());
+    }
+
+    ReadOnlyProperty<ZonedDateTime> endProperty() {
+        return end;
+    }
+
+    public LocalDate getCreateDate() {
+        return createdDate.toLocalDate();
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public ZonedDateTime getLastUpdate() {
+        return ZonedDateTime.ofInstant(lastUpdated.getValue().toInstant(), ZoneId.systemDefault());
+    }
+
+    ReadOnlyProperty<ZonedDateTime> lastUpdatedProperty() {
+        return lastUpdated;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("AppointmentView{");
+        sb.append("createdDate=").append(createdDate);
+        sb.append(", createdBy='").append(createdBy).append('\'');
+        sb.append(", titleProperty=").append(title);
+        sb.append(", descriptionProperty=").append(description);
+        sb.append(", locationProperty=").append(location);
+        sb.append(", contactProperty=").append(contact);
+        sb.append(", urlProperty=").append(url);
+        sb.append(", customerName=").append(customerName);
+        sb.append(", startProperty=").append(start);
+        sb.append(", endProperty=").append(end);
+        sb.append(", lastUpdated=").append(lastUpdated);
+        sb.append('}');
+        return sb.toString();
+    }
 }
--- src/edu/wgu/scheduler/models/CustomerView.java
+++ src/edu/wgu/scheduler/models/CustomerView.java
@@ -1,10 +1,215 @@
 package edu.wgu.scheduler.models;
 
+import javafx.beans.property.*;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static edu.wgu.scheduler.MainApp.dataSource;
+
 /**
- * Created by Ian Cornett - icornet@wgu.edu on 9/2/17.
+ * Created by Ian Cornett - icornet@wgu.edu on 7/30/2017 at 20:52.
  * Part of Project: scheduler
  * <p>
  * Student ID: 000292065
  */
-public class CustomerView {
+public class CustomerView implements ICustomerView {
+
+    private ReadOnlyStringProperty customerName;
+    private ReadOnlyStringProperty address;
+    private ReadOnlyStringProperty address2;
+    private ReadOnlyStringProperty city;
+    private ReadOnlyStringProperty postalCode;
+    private ReadOnlyStringProperty country;
+    private ReadOnlyStringProperty phone;
+    private ReadOnlyBooleanProperty active;
+    private final ZonedDateTime createDate;
+    private final String createdBy;
+    private ReadOnlyStringProperty lastUpdateBy;
+    private ReadOnlyObjectProperty<ZonedDateTime> lastUpdate;
+
+
+    public CustomerView(String customerName, String address, String address2, String city, String postalCode, String country, String phone, boolean active, Timestamp createDate, String createdBy, String lastUpdateBy, Timestamp lastUpdate){
+        this.customerName = new SimpleStringProperty(customerName);
+        this.address = new SimpleStringProperty(address);
+        this.address2 = new SimpleStringProperty(address2);
+        this.city = new SimpleStringProperty(city);
+        this.postalCode = new SimpleStringProperty(postalCode);
+        this.country = new SimpleStringProperty(country);
+        this.phone = new SimpleStringProperty(phone);
+        this.active = new SimpleBooleanProperty(active);
+        this.createDate = createDate.toInstant().atZone(ZoneId.systemDefault());
+        this.createdBy = createdBy;
+        this.lastUpdateBy = new SimpleStringProperty(lastUpdateBy);
+        this.lastUpdate = new SimpleObjectProperty<>(lastUpdate.toInstant().atZone(ZoneId.systemDefault()));
+    }
+
+    public boolean isActive() {
+        return active.get();
+    }
+
+    ReadOnlyStringProperty customerName(){
+        return customerName;
+    }
+
+    public int getAddressId() {
+        try (Connection connection = dataSource.getConnection()){
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT addressId FROM address\n" +
+                    "INNER JOIN city on address.cityId = city.cityId\n" +
+                    "INNER JOIN country on city.countryId = country.countryId\n" +
+                    "WHERE address = ?\n" +
+                    "AND address2 = ?\n" +
+                    "AND city = ?\n" +
+                    "AND country = ?;");
+            statement.setString(1, this.address.toString());
+            statement.setString(2, this.address2.toString());
+            statement.setString(3, this.city.toString());
+            statement.setString(4, this.country.toString());
+
+            ResultSet rs = statement.executeQuery();
+            if(rs.next()){
+                return rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    @Override
+    public int getCustomerId() {
+        try (Connection connection = dataSource.getConnection()){
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT customerId FROM customer\n" +
+                            "INNER JOIN address ON customer.addressId = address.addressId\n" +
+                            "INNER JOIN city ON address.cityId = city.cityId\n" +
+                            "INNER JOIN country ON city.countryId = country.countryId\n" +
+                            "WHERE customerName = ?\n" +
+                            "      AND address = ?\n" +
+                            "      AND address2 = ?\n" +
+                            "      AND city = ?\n" +
+                            "      AND postalCode = ?\n" +
+                            "      AND country = ?");
+            statement.setString(1, this.getCustomerName());
+            statement.setString(2, this.getAddress());
+            statement.setString(3, this.getAddress2());
+            statement.setString(4, this.getCity());
+            statement.setString(5, this.getPostalCode());
+            statement.setString(6, this.getCountry());
+
+            ResultSet rs = statement.executeQuery();
+            if(rs.next()){
+                return rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+
+    public LocalDate getCreateDate() {
+        return this.createDate.toLocalDate();
+    }
+
+    public String getCreatedBy() {
+        return this.createdBy;
+    }
+
+    public String getCustomerName() {
+        return customerName().getName();
+    }
+
+    public String getLastUpdateBy() {
+        return this.lastUpdateBy.getValueSafe();
+    }
+
+    public String getAddress() {
+        return this.address.getValue();
+    }
+
+    ReadOnlyStringProperty address(){
+        return address;
+    }
+
+    public String getAddress2() {
+        return address2.getValueSafe();
+    }
+
+    ReadOnlyStringProperty address2(){
+        return address2;
+    }
+
+    public String getCity() {
+        return city.getValue();
+    }
+
+    ReadOnlyStringProperty city(){
+        return city;
+    }
+
+    public String getCountry() {
+        return country.getValue();
+    }
+
+    ReadOnlyStringProperty country(){
+        return country;
+    }
+
+    public String getPhone() {
+        return phone.getValue();
+    }
+
+    ReadOnlyStringProperty phone(){
+        return phone;
+    }
+
+    public String getPostalCode() {
+        return postalCode.getValueSafe();
+    }
+
+    @Override
+    public ZonedDateTime getLastUpdate() {
+        ZonedDateTime zdt = ZonedDateTime.ofInstant(lastUpdate.getValue().toInstant(), ZoneId.of("UTC"));
+        return zdt.withZoneSameInstant(ZoneId.systemDefault());
+    }
+
+    public ReadOnlyStringProperty postalCode(){
+        return postalCode;
+    }
+
+
+    public ReadOnlyBooleanProperty active() {
+        return active;
+    }
+
+    public ReadOnlyStringProperty lastUpdateByProperty() {
+        return lastUpdateBy;
+    }
+
+    public ReadOnlyObjectProperty<ZonedDateTime> lastUpdateProperty() {
+        return lastUpdate;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("CustomerView{\n");
+        sb.append("\tcustomerName=").append(customerName.getValueSafe());
+        sb.append("\n\t address=").append(address.getValueSafe());
+        sb.append("\n\t address2=").append(address2.getValueSafe());
+        sb.append("\n\t city=").append(city.getValueSafe());
+        sb.append("\n\t postalCode=").append(postalCode.getValueSafe());
+        sb.append("\n\t country=").append(country.getValueSafe());
+        sb.append("\n\t phone=").append(phone.getValueSafe());
+        sb.append("\n\t active=").append(active.getValue());
+        sb.append("\n\t createDate=").append(createDate);
+        sb.append("\n\t createdBy='").append(createdBy).append('\'');
+        sb.append("\n\t lastUpdateBy=").append(lastUpdateBy.getValueSafe());
+        sb.append("\n\t lastUpdate=").append(getLastUpdate());
+        sb.append("\n}");
+        return sb.toString();
+    }
 }
