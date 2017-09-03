f3645bf7d95a6259223f4c0ed7778b6c525c827c
Ian Cornett <icornet@wgu.edu>
2017-09-02 21:23:55 -0700

    unwind unneccessary Property objects

---
+++ src/edu/wgu/scheduler/models/AppointmentView.java
@@ -0,0 +1,10 @@
+package edu.wgu.scheduler.models;
+
+/**
+ * Created by Ian Cornett - icornet@wgu.edu on 9/2/17.
+ * Part of Project: scheduler
+ * <p>
+ * Student ID: 000292065
+ */
+public class AppointmentView {
+}
